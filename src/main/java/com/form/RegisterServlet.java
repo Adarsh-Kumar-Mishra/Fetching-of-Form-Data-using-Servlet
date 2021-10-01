1983b094f2351a60fc1747f61f4e208560e6adae
Adarsh-Kumar-Mishra <adarshmishra360@gmail.com>
2021-10-02 00:28:50 +0530

    Submiting and Fetching data

---
+++ src/main/java/com/form/RegisterServlet.java
@@ -0,0 +1,26 @@
+package com.form;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	      resp.setContentType("text/html");
+    	      PrintWriter out = resp.getWriter();
+    	      out.println("<h2>Welcomr to Register servlet</h2>");
+    	      String name = req.getParameter("user_name");
+    	      String password = req.getParameter("user_password");
+    	      if(name.length()!=0) {
+    	    	  out.println("<h2> Name :"+name+"<h2>");
+    	    	  out.println("<h2> Password :"+password+"<h2>");
+    	    	  
+    	      }else {
+    	    	  out.print("You have not filled the form yet");
+    	      }
+     }
+}
